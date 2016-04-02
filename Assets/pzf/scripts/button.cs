8e7866d42d9b690bd1b89bd187151dcd6efdfe97
Piotr Foltyn <piotr.foltyn@bromium.com>
2016-04-03 00:37:05 +0100

    initial commit

---
+++ Assets/pzf/scripts/button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+
+    static string lang_prefix = "en";
+    static string prev_lang_prefix = "en";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0) {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            if (GetComponent<Collider2D>().OverlapPoint(wp)) {
+                var source = GetComponent<AudioSource>();
+                source.clip.name = source.clip.name.Replace(prev_lang_prefix, lang_prefix);
+                source.Play();
+                if (gameObject.name.StartsWith("lang")) {
+                    prev_lang_prefix = lang_prefix;
+                    lang_prefix = gameObject.name.Split('_')[1];
+                }
+            }
+        }
+    }
+}
---
+++ Assets/pzf/scripts/pilot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pilot : MonoBehaviour {
+
+    void fitCameraWidth()
+    {
+        SpriteRenderer sr = (SpriteRenderer)GetComponent("Renderer");
+        if (sr == null)
+            return;
+
+        // Get stuff
+        double width = sr.sprite.bounds.size.x;
+        double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        double worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        // Resize
+        transform.localScale = new Vector2(1, 1) * (float)(worldScreenWidth / width);
+    }
+
+    // Use this for initialization
+    void Start () {
+        fitCameraWidth();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
