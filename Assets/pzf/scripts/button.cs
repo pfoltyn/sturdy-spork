49b0224f4e51226c9c94ff43fadb1588df6f8a33
Piotr Foltyn <piotr.foltyn@bromium.com>
2016-04-03 11:56:41 +0100

    reduce sound lag

--- Assets/pzf/scripts/button.cs
+++ Assets/pzf/scripts/button.cs
@@ -6,25 +6,27 @@ public class button : MonoBehaviour {
     static string lang_prefix = "en";
 
     string prev_lang_prefix = "en";
+    string postfix;
+    bool skip = false;
     AudioClip clip = null;
+    AudioSource source = null;
 
     // Use this for initialization
     void Start () {
+        source = GetComponent<AudioSource>();
+        postfix = gameObject.name.Split('_')[1];
+        skip = gameObject.name.StartsWith("lang");
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void OnGUI () {
         if (Input.touchCount > 0)
         {
             Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
             if (GetComponent<CircleCollider2D>().OverlapPoint(wp))
             {
-                var source = GetComponent<AudioSource>();
-                var tag = gameObject.name.Split('_')[1];
-                if (gameObject.name.StartsWith("lang"))
+                if (skip)
                 {
-                    lang_prefix = tag;
+                    lang_prefix = postfix;
                 }
                 else
                 {
@@ -33,14 +35,17 @@ public class button : MonoBehaviour {
                         prev_lang_prefix = lang_prefix;
                         if (!clip)
                         {
-                            clip = Resources.Load("sounds/" + lang_prefix + "_" + tag) as AudioClip;
+                            clip = Resources.Load("sounds/" + lang_prefix + "_" + postfix) as AudioClip;
                         }
                         var tmp = source.clip;
                         source.clip = clip;
                         clip = tmp;
                     }
                 }
-                source.Play();
+                if (!source.isPlaying) {
+                    source.timeSamples = 3000;
+                    source.Play();
+                }
             }
         }
     }
